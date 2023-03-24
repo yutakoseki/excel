3c214d896a2896881a71c37e9515193b0fbe7828
koseki <koseki@thinkingreed.co.jp>
2023-03-24 21:08:02 +0900

    全件取得加工

--- import.php
+++ import.php
@@ -8,16 +8,36 @@ $fileName = 'excel/import.xlsx';
 $spreadsheet = $reader->load($fileName);
 
 // シートの読み込み
-$sheet = $spreadsheet->getActiveSheet();
+// $sheet = $spreadsheet->getActiveSheet();
 
-// シートの取得
+// シートの総数取得
 $sheetCount = $spreadsheet->getSheetCount();
+
+// シート毎にデータを取得
 for ($i=0; $i < $sheetCount; $i++) {
     $sheet = $spreadsheet->getSheet($i);
-    // 値の取得
     $valueAll[] = $sheet->rangeToArray('C6:H50');
-    print_r('<pre>'); print_r($valueAll[$i]); print_r('</pre>');
-    echo "_________________________________________________________";
-    echo "<br>";
+    $ingredient = array();
+    // 食材ごとにデータをトリム
+    for ($j=0; $j < count($valueAll[$i]); $j++) {
+        $ingredientName = trim(mb_convert_kana($valueAll[$i][$j][0], "s"));
+        $gram = trim($valueAll[$i][$j][2]);
+        $comment = trim($valueAll[$i][$j][5]);
+        $ingredient[$j] = array("食材名"=>$ingredientName,"一人当たりg"=>$gram,"コメント"=>$comment);
+    }
+    $ingredientList[$i] = $ingredient;
+    print_r('<pre>'); print_r($ingredientList); print_r('</pre>');
 }
+
+
+// シートの取得
+// $sheetCount = $spreadsheet->getSheetCount();
+// for ($i=0; $i < $sheetCount; $i++) {
+//     $sheet = $spreadsheet->getSheet($i);
+//     // 値の取得
+//     $valueAll[] = $sheet->rangeToArray('C6:H50');
+//     print_r('<pre>'); print_r($valueAll[$i]); print_r('</pre>');
+//     echo "_________________________________________________________";
+//     echo "<br>";
+// }
 
